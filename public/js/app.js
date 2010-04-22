bed419a562220f278d6e3d4364159718ac67f9ed
Dylan Clendenin <dylan.clendenin@gmail.com>
2010-04-22 14:25:57 -0700

    limit maxsize of textarea and disable \n chars

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $("textarea").focus()
     $("input.copynpaste").focus()
+    
+    $(".blowitup").keyup(function(k){
+        if (k.keyCode == 13) {
+            $("textarea").val($("textarea").val().replace("\n",""))
+        }
+    })
 })
