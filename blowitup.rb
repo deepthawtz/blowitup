7b92c9b7b0c01774b3675da4c143a8bd851985bc
Dylan Clendenin <dylan.clendenin@gmail.com>
2011-02-24 11:32:58 -0800

    - 404 page - fixed bug on stats page

--- blowitup.rb
+++ blowitup.rb
@@ -37,18 +37,23 @@ module Blowitup
       redis.bgsave
 
       haml :paste, :locals => {
-        :url => "/message/#{message_sha}"
+        :url => "message/#{message_sha}"
       }
     end
 
     get "/message/:sha" do
-      message = Marshal.load(redis.hget "blowitup:messages", params[:sha])
-      redis.hincrby "blowitup:messages:views", params[:sha], 1
+      begin
+        m = redis.hget "blowitup:messages", params[:sha]
+        message = Marshal.load(m)
+        redis.hincrby "blowitup:messages:views", params[:sha], 1
+        haml :show, :layout => false, :locals => {
+          :message => message[:message],
+          :style => message[:style]
+        }
+      rescue TypeError
+        haml :"404"
+      end
 
-      haml :show, :layout => false, :locals => {
-        :message => message[:message],
-        :style => message[:style]
-      }
     end
 
     get "/huh" do
@@ -61,7 +66,7 @@ module Blowitup
       redis.hgetall("blowitup:messages").each do |k,v|
         m = Marshal.load v
         views = redis.hget "blowitup:messages:views", k
-        report << "<li><a href='/message/#{k}'>#{k}</a> || <em>#{m[:created].strftime("%m-%d-%Y [%I:%M%p]")}</em> || <b>#{m[:message]}</b> || <span class='views'>#{views || 0} views</span>"
+        report << "<li><a href='/message/#{k}'>#{k}</a> || <em>#{m[:created].strftime("%m-%d-%Y [%I:%M%p]")}</em> || <b>#{m[:message]}</b> || <span class='views'>#{(views || 0)} views</span>"
       end
 
       haml :stats, :locals => {
